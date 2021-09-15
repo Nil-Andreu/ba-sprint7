3f24135fc4cf3dcdfa9efaeeab49e7eab2809de6
Nil Andreu <nilandreug@gmail.com>
2021-09-15 17:34:23 +0200

    Exercici 6

--- src/escena/Escenes.js
+++ src/escena/Escenes.js
@@ -1,7 +1,7 @@
 import React, { useState, Fragment } from "react";
 import styled from "styled-components";
 import Escena from "./Escena";
-import * as data from "../escenes.json";
+import data from "../escenes";
 
 const b1 = data[0][1];
 const b2 = data[1][1];
@@ -12,6 +12,21 @@ const b4 = data[0][1];
 // (as the value () would make to be executed when is rendered)
 function Escenes() {
   const [state, setState] = useState(0);
+
+  const imageRenderer = () => {
+    if (state === 0) {
+      return <Image src={data[0]["img"]} />;
+    }
+    if (state === 1) {
+      return <Image src={data[1]["img"]} />;
+    }
+    if (state === 2) {
+      return <Image src={data[2]["img"]} />;
+    }
+    if (state === 3) {
+      return <Image src={data[3]["img"]} />;
+    }
+  };
 
   const passer = (number) => {
     if (number === 0) {
@@ -28,54 +43,47 @@ function Escenes() {
       }
     }
   };
+
   return (
     <div>
-      (
-      {() => {
-        if (state === 0) {
-          console.log(b1);
-        }
-      }}
-      )
+      {imageRenderer()}
       <Buttons>
         <Button onClick={() => passer(0)}>Anterior</Button>
         <Button onClick={() => passer(1)}>Seguent</Button>
       </Buttons>
       {state === 0 ? (
-        <Escena text={data[0][0]} class={"red"}></Escena>
+        <Escena text={data[0]["text"]} class={"red"}></Escena>
       ) : (
-        <Escena text={data[0][0]}></Escena>
+        <Escena text={data[0]["text"]} class={"white"}></Escena>
       )}
       {state === 1 ? (
-        <Escena text={data[1][0]} class={"red"}></Escena>
+        <Escena text={data[1]["text"]} class={"red"}></Escena>
       ) : (
-        <Escena text={data[1][0]}></Escena>
+        <Escena text={data[1]["text"]} class={"white"}></Escena>
       )}
       {state === 2 ? (
-        <Escena text={data[2][0]} class={"red"}></Escena>
+        <Escena text={data[2]["text"]} class={"red"}></Escena>
       ) : (
-        <Escena text={data[2][0]}></Escena>
+        <Escena text={data[2]["text"]} class={"white"}></Escena>
       )}
       {state === 3 ? (
-        <Escena text={data[3][0]} class={"red"}></Escena>
+        <Escena text={data[3]["text"]} class={"red"}></Escena>
       ) : (
-        <Escena text={data[3][0]}></Escena>
+        <Escena text={data[3]["text"]} class={"white"}></Escena>
       )}
     </div>
   );
 }
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  z-index: -5;
-`;
-
-const Background = styled.div`
-  z-index: -1;
+const Image = styled.img`
   width: 100vw;
   height: 100vh;
   position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
 `;
 
 const Buttons = styled.div`
---
+++ src/escenes.js
@@ -0,0 +1,24 @@
+import img1 from "./img/1.jpg";
+import img2 from "./img/2.jpg";
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.jpg";
+
+// Remember that for rendering images we need to do like this
+export default [
+  {
+    text: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    img: img1,
+  },
+  {
+    text: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    img: img2,
+  },
+  {
+    text: "L'heroi va decidir travessar la porta que el portava a casa",
+    img: img3,
+  },
+  {
+    text: "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
+    img: img4,
+  },
+];
